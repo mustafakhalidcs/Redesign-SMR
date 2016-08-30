0e6072d06d595d0b6a22f26448121d41fa88ef93
mustafakhalidcs <mustafakhalidcs@gmail.com>
2016-08-30 18:18:19 +0500

    working on signup form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -176,7 +176,36 @@ angular.module('starter.controllers', ['oitozero.ngSweetAlert'])
 })
 
 .controller('signin', function($scope) {})
-.controller('SignupCtrl', function($scope) {})
+.controller('SignupCtrl', function($scope,$http) {
+  $scope.fname = "test";
+  $scope.createUser=function(){
+    console.log("HEllo from create user");
+    console.log($scope.fname);
+    $http({
+          method: 'POST',
+          data: $.param(
+          {
+           'first_name': $scope.fname, 
+           'last_name': $scope.lname, 
+           'mobile': $scope.mobileno ,
+           'resident_of': $scope.resident_of ,
+           'email': $scope.email , 
+           'password': $scope.password ,
+           'nic': $scope.nic ,
+           "command":"createUser"
+         }
+         ),
+          headers : {
+                'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+                    },
+          url: "http://localhost:8080/ShareMyRide/signup.php"
+          }).success(function(data,status,headers,config){
+            console.log(console.log($scope.fname));
+          });
+  }
+  
+  
+})
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
 
